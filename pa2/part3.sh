27b23ecd3abb37957129f89baf9c8496428bb068
j <washwor1@vols.utk.edu>
2023-03-20 20:21:53 -0400

    did part3

--- pa2/part3.sh
+++ pa2/part3.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-
-# Your solution for part 3 goes here.
+# awk is op
+# did the same thing as part2, but used awk.
+# I did the iterative approach here because it made more sense
+awk -F ',' 'BEGIN {max_hp=0;} NR>1 {if($6>max_hp){max_hp=$6; make=$1}} END {printf "%s %.6f\n", make, max_hp > "answer.txt"}' cars.csv
