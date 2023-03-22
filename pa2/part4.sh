5f60e4742637eefb52cdadc306f22a1e322a1ef0
j <washwor1@vols.utk.edu>
2023-03-21 20:22:49 -0400

    finished part 4

--- pa2/part4.sh
+++ pa2/part4.sh
@@ -1,3 +1,32 @@
 #!/usr/bin/env bash
+#basically used the "general strategy"
+lines=()
 
-# Your solution for part 4 goes here.
+for file in ./*.sample; do
+  awk -F'[[:space:]:]+' '/^sequence position/ {number=$3} /^sequence data/ {string=$3; for (i=4; i<=NF; i++) string=string""$i; formatted=sprintf("%04d %s", number, string); print formatted}' "$file"
+done |
+sort -n |
+cut -d ' ' -f 2 |
+tr -d '\n' > sequence.txt
+
+
+fold -w1 sequence.txt | awk '
+  { count[toupper($0)]++ }
+  END {
+    print "A " count["A"]
+    print "T " count["T"]
+    print "C " count["C"]
+    print "G " count["G"]
+  }
+' > pairs.txt
+
+sequence=$(cat sequence.txt)
+
+fold -w3 sequence.txt | awk '
+  { count[$0]++ }
+  END {
+    for (codon in count) {
+      printf "%s %.6f%%\n", codon, count[codon]/(length("'"$sequence"'")/3)*100
+    }
+  }
+' | sort > codons.txt
